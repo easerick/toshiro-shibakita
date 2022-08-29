6a8adeeb46ab6b55e40ca1af47340e6ed46bbbd3
easerick <ericksvip@gmail.com>
2022-08-29 02:38:04 +0000

    toshiro-shibakita

---
+++ banco.sql
@@ -0,0 +1,8 @@
+CREATE TABLE listaprodutos (
+    ProdutoID int,
+    Produto varchar(30),
+    Marca varchar(10),
+    Classificacao varchar(25),
+    Apresentacao varchar(10),
+    Massa varchar(10),
+);
